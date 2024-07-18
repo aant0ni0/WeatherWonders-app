1f5aa922c4ebb006ba69d04c53d3c9549f5d71c8
Antoni Michalczak <antoni0michalczak@gmail.com>
2024-07-18 15:33:34 +0200

    Add bottomTabs and FiveDaysScreen

--- App.tsx
+++ App.tsx
@@ -1,25 +1,60 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import { RootStackParamList } from "./types/navigation";
+import { RootStackParamList, RootTabsParamList } from "./types/navigation";
 import SingleDayScreen from "./screens/SingleDayScreen";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import FiveDaysScreen from "./screens/FiveDaysScreen";
 
 const Stack = createStackNavigator<RootStackParamList>();
+const Tab = createBottomTabNavigator<RootTabsParamList>();
+
+const BottomTabNavigator = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="SingleDay"
+        component={SingleDayScreen}
+        options={{
+          title: "Today",
+          headerShown: false,
+        }}
+      />
+      {/* <Tab.Screen
+        name="SingleDay"
+        component={SingleDayScreen}
+        options={{
+          title: "Tommorow",
+        }}
+      /> */}
+      <Tab.Screen
+        name="FiveDays"
+        component={FiveDaysScreen}
+        options={{
+          title: "In five days",
+          headerShown: false,
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
 
 export default function App() {
   return (
     <>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen
-            name="SingleDay"
-            component={SingleDayScreen}
-            options={{ headerShown: false }}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <SafeAreaView style={styles.container}>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="Tabs"
+              component={BottomTabNavigator}
+              options={{ headerShown: false }}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </SafeAreaView>
     </>
   );
 }
@@ -27,8 +62,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
 });
---
+++ screens/FiveDaysScreen.tsx
@@ -0,0 +1,87 @@
+import { View, Text, ActivityIndicator } from "react-native";
+import { useState, useEffect } from "react";
+import type { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { RootStackParamList } from "../types/navigation";
+import { getWeatherByCity, getForecastByCity } from "../services/api";
+import {
+  ForecastData,
+  ForecastItem,
+  WeatherData,
+} from "../types/weatherSchema";
+
+const FiveDaysScreen = () => {
+  const [forecastData, setForecastData] = useState<ForecastData | null>(null);
+  const [weatherData, setWeatherData] = useState<WeatherData | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+      try {
+        const city = "Warsaw";
+        const [weather, forecast] = await Promise.all([
+          getWeatherByCity(city),
+          getForecastByCity(city),
+        ]);
+        setWeatherData(weather);
+        setForecastData(forecast);
+      } catch (err) {
+        console.log(err);
+        setError("Error fetching weather data");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchWeatherData();
+  }, []);
+
+  const getDailyForecastForNext5Days = (): ForecastItem[] => {
+    if (!forecastData) return [];
+
+    const dailyForecasts: { [date: string]: ForecastItem } = {};
+
+    forecastData.list.forEach((item: ForecastItem) => {
+      const date = new Date(item.dt * 1000).toLocaleDateString();
+
+      if (
+        !dailyForecasts[date] ||
+        item.main.temp > dailyForecasts[date].main.temp
+      ) {
+        dailyForecasts[date] = item;
+      }
+    });
+
+    const sortedForecasts = Object.values(dailyForecasts).sort(
+      (a, b) => a.dt - b.dt
+    );
+
+    return sortedForecasts.slice(0, 5);
+  };
+
+  const dailyForecast = getDailyForecastForNext5Days();
+
+  if (loading) {
+    return (
+      <View>
+        <ActivityIndicator size="large" color="#0000ff" />
+      </View>
+    );
+  }
+
+  if (error) {
+    return (
+      <View>
+        <Text>Error fetching weather data: {error}</Text>
+      </View>
+    );
+  }
+
+  return (
+    <View>
+      <Text>FiveDaysScreen</Text>
+    </View>
+  );
+};
+
+export default FiveDaysScreen;
